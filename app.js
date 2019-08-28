7fc4b609fe6e420c2c8374d0c16173dd86d35911
Krzysztof Bistram (PGS Software) <kbistram@pgs-soft.com>
2019-08-28 15:12:20 +0200

    Added databinding

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el: '#vue-app',
   data: {
     name: 'Janusz',
-    job: 'Złodziej'
+    job: 'Złodziej',
+    website: 'http://google.pl',
+    websiteTag: '<a href="http://google.pl"> GGGGG </a>'
   },
   methods: {
     greet: function () {
